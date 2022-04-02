71d88083bcffc4166f0531d07891a3cb1a615549
Bayastan14 <99891247+Bayastan14@users.noreply.github.com>
2022-04-02 09:31:12 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import javax.sql.rowset.CachedRowSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+   ArrayList<String> a = new ArrayList<>();
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Введите 5 имен для списка А");
+
+        a.add(scanner.nextLine());
+        a.add(scanner.nextLine());
+        a.add(scanner.nextLine());
+        a.add(scanner.nextLine());
+        a.add(scanner.nextLine());
+
+        System.out.println(a.toString());
+
+
+
+        ArrayList<String> b = new ArrayList<>();
+
+        System.out.println("Введите 5 имен для списка Б");
+
+        System.out.println("---------------");
+
+
+        b.add(scanner.nextLine());
+        b.add(scanner.nextLine());
+        b.add(scanner.nextLine());
+        b.add(scanner.nextLine());
+        b.add(scanner.nextLine());
+        System.out.println(b.toString());
+
+        System.out.println("----------------------");
+
+
+        ArrayList<String> c = new ArrayList<>();
+
+        Collections.reverse(b);
+        for (int i  = 0; i <a.size(); i++) {
+            c.add(a.get(i));
+            c.add(b.get(i));
+
+        }
+
+        System.out.println(c);
+
+        System.out.println("--------------------");
+
+
+        StringComparator stringComparator = new StringComparator();
+        Collections.sort(c,stringComparator);
+        System.out.println(c);
+    }
+        }
+
+
